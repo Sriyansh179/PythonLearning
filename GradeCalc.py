bb794ccbc5a4cc25de81cc6f2f40db1f1043c655
Sriyansh179 <sriyanshsenapati@gmail.com>
2025-07-02 19:58:05 -0500

    Add files via upload
    Grade Calculator

---
+++ GradeCalc.py
@@ -0,0 +1,12 @@
+marks = int(input("Enter marks recieved: \n"))
+
+percentage = (marks/150)*100
+if percentage >= 90:
+    print("Your grade: A")
+elif percentage >= 80:
+    print("Your grade: B")
+elif percentage >= 70:
+    print("Your grade: C")
+else:
+    print("Your grade: F")
+    
