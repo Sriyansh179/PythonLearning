28acc242356e5bab870ce3236630a292fe2371c0
Sriyansh179 <sriyanshsenapati@gmail.com>
2025-09-19 22:26:41 -0500

    Update ShoppingCart.py
    You can now remove items from the cart.

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -1,32 +1,63 @@
-stock = {"apple": [20, 2], "orange": [30, 3], "banana": [10, 4], "pineapple": [15, 4], "papaya": [20, 3], "strawberry": [25, 5]}
-cart = []
+stock = {"apple": {"stock": 20, "price": 2}, "orange": {"stock": 30, "price": 3}, "banana": {"stock": 10, "price": 4}, "pineapple": {"stock": 15, "price": 4}, "papaya": {"stock": 20, "price": 3}, "strawberry": {"stock": 25, "price": 5}}
+cart = {}
 cost = 0
-decision2 = "yes"
 
 decision = input("Do you want to shop? ").lower()
 
 if decision == "yes":
     print("Items available: ")
     for k in stock:
-        print(k + ": $" + str(stock[k][1]) + "\nStock available: " + str(stock[k][0]) + "\n------------------------")
-    
-    
-    while decision2 == "yes":
-        item = input("Which fruit do you want? ").lower()
+        print(k + ": $" + str(stock[k]["price"]) + "\nStock available: " + str(stock[k]["stock"]) + "\n------------------------")
 
-        if item in stock:
-            cart.append(item)
-            cost = cost + stock[item][1]
+    while True:
+        decision2 = input("Do you want to add/remove items or exit? ")
+
+        if decision2 == "add":
+            item = input("Which fruit do you want? ").lower()
+
+            if item in cart:
+                quantity = int(input("How many " + item + "s do you want? "))
+                cart[item] += quantity
+                cost = cost + ((stock[item]["price"])*quantity)
+
+            elif item in stock:
+                quantity = int(input("How many " + item + "s do you want? "))
+                cart.update({item: quantity})
+                cost = cost + ((stock[item]["price"])*quantity)
+            else:
+                print("Item not in stock.")
+
             print("Your cart: " + str(cart))
-        else:
-            print("Item not in stock.")
-        decision2 = input("Do you want to add more items to your cart? ").lower()
+            print("Your total cost: $" + str(cost))
+            
 
-    print("Your cart: " + str(cart))
-    print("Your total cost: $" + str(cost))
+        elif decision2 == "remove":
+            removeItem = input("Which item do you want to remove? ").lower()
+
+            if removeItem in cart:
+                removeQuantity = int(input("How many " + removeItem + "s do you want to remove? "))
+
+                if removeQuantity > cart[removeItem]:
+                    print("Can't remove more items than already in cart. ")
+
+                else:
+                    cart[removeItem] -= removeQuantity
+                    cost = cost - ((stock[removeItem]["price"])*removeQuantity)
+                
+            else:
+                print("Item not added in cart previously.")
+            
+
+            print("Your cart: " + str(cart))
+            print("Your total cost: $" + str(cost))
+
+        else:
+            break
+
+    print("Your final cart: " + str(cart))
+    print("Your final cost: $" + str(cost))
         
 else:
     print("Come back later.")
-
 
 
