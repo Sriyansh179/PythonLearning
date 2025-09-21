c10e547656c964e8fe0b40617136b4dbb22124ba
Sriyansh179 <sriyanshsenapati@gmail.com>
2025-09-20 21:39:45 -0500

    Update3 ShoppingCart.py
    Implemented addition and removal of multiple items simultaneously.

--- ShoppingCart.py
+++ ShoppingCart.py
@@ -2,34 +2,36 @@ stock = {"apple": {"stock": 20, "price": 2}, "orange": {"stock": 30, "price": 3}
 cart = {}
 cost = 0
 
-def funcAdd(fruit, quantity):
+def funcAdd(**kwargs):
     global cost
-    if fruit in cart:
-        cart[fruit] += quantity
-        cost = cost + ((stock[fruit]["price"])*quantity)
+    for fruit, quantity in kwargs.items():
+        if fruit in cart:
+            cart[fruit] += quantity
+            cost = cost + ((stock[fruit]["price"])*quantity)
 
-    elif fruit in stock:
-        cart.update({fruit: quantity})
-        cost = cost + ((stock[fruit]["price"])*quantity)
-    else:
-        print("Item not in stock.")
+        elif fruit in stock:
+            cart.update({fruit: quantity})
+            cost = cost + ((stock[fruit]["price"])*quantity)
+        else:
+            print("Item not in stock.")
 
     print("Your cart: " + str(cart))
     print("Your total cost: $" + str(cost))
 
 
-def funcRemove(rFruit, rQuantity):
+def funcRemove(**rKwargs):
     global cost
-    if rFruit in cart:
-        if rQuantity > cart[rFruit]:
-             print("Can't remove more items than already in cart. ")
+    for rFruit, rQuantity in rKwargs.items():
+        if rFruit in cart:
+            if rQuantity > cart[rFruit]:
+                 print("Can't remove more items than already in cart. ")
 
-        else:
-            cart[rFruit] -= rQuantity
-            cost = cost - ((stock[rFruit]["price"])*rQuantity)
+            else:
+                cart[rFruit] -= rQuantity
+                cost = cost - ((stock[rFruit]["price"])*rQuantity)
                 
-    else:
-        print("Item not added in cart previously.")
+        else:
+            print("Item not added in cart previously.")
             
 
     print("Your cart: " + str(cart))
@@ -47,14 +49,26 @@ if decision == "yes":
         action = input("Do you want to add/remove items or exit? ")
 
         if action == "add":
-            fruit = input("Which fruit do you want to add? ").lower()
-            quantity = int(input("How many " + fruit + "s do you want to add? "))
-            funcAdd(fruit, quantity)
+            numAdd = int(input("Enter the number of fruits you want to add: "))
+            addDict = {}
+
+            for i in range(numAdd):
+                key = input("Enter fruit: ")
+                value = int(input("Enter quantity: "))
+                addDict[key] = value
+
+            funcAdd(**addDict)
 
         elif action == "remove":
-            rFruit = input("Which fruit do you want to remove? ").lower()
-            rQuantity = int(input("How many " + fruit + "s do you want to remove? "))
-            funcRemove(rFruit, rQuantity)
+            numRemove = int(input("Enter the number of fruits you want to remove: "))
+            removeDict = {}
+
+            for k in range(numRemove):
+                rKey = input("Enter fruit: ")
+                rValue = int(input("Enter quantity: "))
+                removeDict[rKey] = rValue
+
+            funcRemove(**removeDict)
 
         else:
             break
@@ -64,4 +78,3 @@ if decision == "yes":
 
 else:
     print("Come back later.")
-
